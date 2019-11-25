a47c1a0f6080258b792d87c1db6dce9faa6b6a73
N1ght420 <n1ghtpe0ple@protonmail.com>
2019-11-25 22:02:07 +0700

    Update net.sh

--- net.sh
+++ net.sh
@@ -21,27 +21,73 @@ echo -e "$white     /___/_.___/_/____/$red _/ |_/_/  /_/   "
 echo -e ""
 echo -e "$white       Ibis-Linux_ Network Manager"
 echo -e ""
-read -p "Interface : " interface;
-ifconfig $interface up
-iwlist $interface scan | grep 'Frequency:\|Quality=\|Encryption\|ESSID:' | sed "s/=/:/" | sed "s/                    //" | sed "s/Signal level=/\nSignal:/" | sed "s/ (Channel /\nChannel:/" | sed "s/)//" | sed "s/Frequency:/\nFrequency:/"
-echo -e ""
-read -p "ESSID : " essid;
-read -p "Pass  : " pass;
-echo -e ""
-echo -e "$red //$white Connecting ..."
-if [ $pass == '' ]
+read -p " NM : " act;
+if [ $act == 'connect' ]
 then
-    iwconfig $interface essid "$essid"
-    dhcpcd $interface
+    read -p " Interface : " interface;
+    ifconfig $interface up
+    iwlist $interface scan | grep 'Frequency:\|Quality=\|Encryption\|ESSID:' | sed "s/=/:/" | sed "s/                    //" | sed "s/Signal level=/\nSignal:/" | sed "s/ (Channel /\nChannel:/" | sed "s/)//" | sed "s/Frequency:/\nFrequency:/"
+    echo -e ""
+    read -p " ESSID : " essid;
+    read -p " Pass  : " pass;
+    echo -e ""
+    if [ $pass == '' ]
+    then
+        echo -e "$red //$white Connecting to $essid ..."
+        iwconfig $interface essid "$essid"
+        echo -e "$red //$white Authenticating ..."
+        dhcpcd $interface
+    else
+        echo -e "$red //$white Creating wpa_passhrase config ..."
+        wpa_passphrase "$essid" $pass > ~/.config/wpa/ssid.conf
+        echo -e "$red //$white Connecting to $essid ..."
+        wpa_supplicant -B -i $interface -c ~/.config/wpa/ssid.conf -D wext
+        echo -e "$red //$white Authenticating ..."
+        dhcpcd $interface
+    fi
+    ip=`ifconfig $interface | grep 'inet ' | awk '{print $2}'`
+    if [ $ip == '' ]
+    then
+        echo -e "$yellow !!$white Cannot obtain IP Address"
+    else
+        echo -e "$okegreen **$white Success obtain IP Address ..."
+        echo -e "$okegreen **$white Your local IP : $ip"
+    fi
+    echo -e "$red //$white Checking connection ..."
+    wget -q --tries=10 --timeout=20 --spider http://google.com
+    if [[ $? -eq 0 ]]
+    then
+        echo -e "$okegreen **$white Connected to Internet"
+    else
+        echo -e "$yellow !!$white Not Connected to Internet"
+    fi
+    rm -rf ~/.config/wpa/ssid.conf
+elif [ $act == 'disconnect' ]
+then
+    echo ''
+elif [ $act == 'status' ]
+then
+    ssid=`iwconfig wlan0 | grep ESSID | awk '{print $4}' | sed "s/ESSID://"`
+    if [ $ssid == 'off/any' ]
+    then
+        echo -e "$yellow !!$white Not Connected to any routers"
+    else
+        ip=`ifconfig $interface | grep 'inet ' | awk '{print $2}'`
+        echo -e "$okegreen **$white Connected to $ssid"
+        echo -e "$okegreen **$white Your IP : $ip"
+    fi
+elif [ $act == 'exit' ]
+then
+    echo -e "$yellow !!$white Exitting"
+    exit
 else
-    wpa_passphrase "$essid" $pass > ~/.config/wpa/ssid.conf
-    wpa_supplicant -B -i $interface -c ~/.config/wpa/ssid.conf -D wext
-    dhcpcd $interface
+    echo -e ""
+    echo -e "$yellow !!$white Command $act not found"
+    echo -e "$red //$white Available commands :"
+    echo -e ""
+    echo -e "$yellow    ->$white connect"
+    echo -e "$yellow    ->$white disconnect"
+    echo -e "$yellow    ->$white status"
+    echo -e "$yellow    ->$white exit"
+    echo -e ""
 fi
-wget -q --tries=10 --timeout=20 --spider http://google.com
-if [[ $? -eq 0 ]]; then
-        echo -e "$okegreen **$white Connected"
-else
-        echo -e "$red !!$white Not Connected"
-fi
-rm -rf ~/.config/wpa/ssid.conf
